21d5f1e3bdcca82c3b003352cf2cfd02892c2f79
yxyx136 <yxyx136@users.noreply.github.com>
2014-01-06 00:44:59 +0100

    replaced bad eval

--- api/world/Animation.js
+++ api/world/Animation.js
@@ -1,6 +1,6 @@
 World.prototype.animateElement = function(e, attr, speed) {
 	e.style.webkitTransition = speed+'s ease';
 	for(var i in attr) {
-		eval('e.style.'+i+'="'+attr[i]+'"');
+		e.style[i] = attr[i];
 	}
 }
